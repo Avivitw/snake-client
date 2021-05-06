628f67a64085634a0ebaadb2faabdc127b9b8e06
Avivit Weissman <wavivit@gmail.com>
2021-05-06 02:08:41 +0000

    passed connection to input model and used it to send the commands to the server

--- input.js
+++ input.js
@@ -1,23 +1,36 @@
-const connect = require("./client");
+// Stores the active TCP connection object.
+let connection;
 
 // setup interface to handle user input from stdin
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", (key) => {
-    handleUserInput(key);
-  });
-    return stdin;
-  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const commands = {
+  'w': 'Move: up',
+  'a': 'Move: left',
+  's': 'Move: down',
+  'd': 'Move: right'
   
-  const handleUserInput = function(key) {
+};
+  
+const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
+  console.log('key', key);
   
+  if (commands[key]) {
+    console.log('commands[key]', commands[key]);
+    connection.write(commands[key]);
+  }
 };
 
 
